b7116079782529068157b1433722ac9b982784b4
Tauan Marcel <tauanmarcel@hotmail.com>
2019-09-22 00:08:42 -0300

    Backend do GoBarber

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,43 @@
+import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
+
+class Appointments extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                date: Sequelize.DATE,
+                canceled_at: Sequelize.DATE,
+                past: {
+                    type: Sequelize.VIRTUAL,
+                    get() {
+                        return isBefore(this.date, new Date());
+                    }
+                },
+                cancelable: {
+                    type: Sequelize.VIRTUAL,
+                    get() {
+                        return isBefore(subHours(new Date(), 2), this.date);
+                    }
+                }
+            },
+            {
+                sequelize
+            }
+        );
+
+        return this;
+    }
+
+    static associate(models) {
+        this.belongsTo(models.User, {
+            foreignKey: 'user_id',
+            as: 'user'
+        });
+        this.belongsTo(models.User, {
+            foreignKey: 'provider_id',
+            as: 'provider'
+        });
+    }
+}
+
+export default Appointments;
---
+++ src/config/mail.js
@@ -0,0 +1,20 @@
+export default {
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: false,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>'
+    }
+};
+
+// Emails Servers:
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+// GMail
+// Mailtrap (DEV)
